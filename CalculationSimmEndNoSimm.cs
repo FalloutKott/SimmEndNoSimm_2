9905232786084de151acab3365a1574f48dbb7e3
KA82 <30778886+KA82@users.noreply.github.com>
2020-03-17 19:54:59 +0300

    перенос методов в отдельный класс

--- CalculationSimmEndNoSimm.cs
+++ CalculationSimmEndNoSimm.cs
@@ -6,7 +6,24 @@ namespace SimmEndNoSimm_2
     public class CalculationSimmEndNoSimm
     {
 
-        
+        public static void CalculationSymmetricalLoad() // Расчёты СИММ // P = 3*Uф*I* cos(φ) = 1,73Uл*I* cos(φ)
+        {
+            double IL1 = Convert.ToDouble(TBL1.Text);
+            double U1 = Convert.ToDouble(TBU1.Text);
+            double result = 1.73 * U1 * IL1;                     // формула 
+            ResultLable.Text = $"{result / 1000} kWt";
+        }
+        public static void CalculationNoSymmetricalLoad() // Расчёты НеСИММ // Pобщ = Ua*Ia* cos(φ1) + Ub*Ib* cos(φ2) + Uc*Ic* cos(φ3)
+        {
+            double IL1 = Convert.ToDouble(TBL1.Text);
+            double IL2 = Convert.ToDouble(TBL2.Text);
+            double IL3 = Convert.ToDouble(TBL3.Text);
+            double U1 = Convert.ToDouble(TBU1.Text);
+            double U2 = Convert.ToDouble(TBU2.Text);
+            double U3 = Convert.ToDouble(TBU3.Text);
+            double result = ((U1 * IL1) + (U2 * IL2) + (U3 * IL3)); // формула 
+            ResultLable.Text = $"{result / 1000} kWt";
+        }
 
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -18,28 +18,11 @@ namespace SimmEndNoSimm_2
 
         }
 
-        public void CalculationSymmetricalLoad() // Расчёты СИММ // P = 3*Uф*I* cos(φ) = 1,73Uл*I* cos(φ)
-        {
-            double IL1 = Convert.ToDouble(TBL1.Text);
-            double U1 = Convert.ToDouble(TBU1.Text);
-            double result = 1.73 * U1 * IL1;                     // формула 
-            ResultLable.Text = $"{result / 1000} kWt";
-        }
-        public void CalculationNoSymmetricalLoad() // Расчёты НеСИММ // Pобщ = Ua*Ia* cos(φ1) + Ub*Ib* cos(φ2) + Uc*Ic* cos(φ3)
-        {
-            double IL1 = Convert.ToDouble(TBL1.Text);
-            double IL2 = Convert.ToDouble(TBL2.Text);
-            double IL3 = Convert.ToDouble(TBL3.Text);
-            double U1 = Convert.ToDouble(TBU1.Text);
-            double U2 = Convert.ToDouble(TBU2.Text);
-            double U3 = Convert.ToDouble(TBU3.Text);
-            double result = ((U1 * IL1) + (U2 * IL2) + (U3 * IL3)); // формула 
-            ResultLable.Text = $"{result / 1000} kWt";
-        }
+        
 
 
 
-        private void ResultBtn_Click(object sender, EventArgs e) // Событие по нажатию кнопки Результат
+        public  void ResultBtn_Click(object sender, EventArgs e) // Событие по нажатию кнопки Результат
         {
             if (ChoiceLoad.Checked)
             {
@@ -51,7 +34,7 @@ namespace SimmEndNoSimm_2
             }
         }
 
-        private void ChoiceLoad_CheckedChanged(object sender, EventArgs e) // Чекбокс // Создать события исключения !!! 
+        public void ChoiceLoad_CheckedChanged(object sender, EventArgs e) // Чекбокс // Создать события исключения !!! 
         {
             if (ChoiceLoad.Checked)
             {
